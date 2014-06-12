31efd6e0ec58381311c3bd323b423be17b603158
Alvaro Segura Del Barco <alvaro.segura.del-barco@hp.com>
2014-06-12 17:04:55 -0600

    not working using the XprotolabController class

--- scope.py
+++ scope.py
@@ -1,20 +1,27 @@
 #!/usr/bin/env python3
 import serial
+import xprotolab_controller
+
+test = xprotolab_controller.XprotolabController()
+
+print(test.__doc__)
+#test.connect
+#print(test.show_version)
 
 #declare serial port and configuration
 #~ print(serial.VERSION)
-ser = serial.Serial()
-ser.baudrate = 115200
-ser.port = '/dev/ttyUSB0'
-ser.bytesize = 8
-ser.parity = 'N'
-ser.stopbits = 1
-
-ser.open()
-if ser.isOpen():
-    print("Connected to port", ser.name)
-    ser.write(b'r') #write only accepts bytes
-    print(ser.readline(256)) #amount of bytes to read are in the xprotolab manual
-    ser.close()
-else:
-    ser.close()
+#ser = serial.Serial()
+#ser.baudrate = 115200
+#ser.port = '/dev/ttyUSB0'
+#ser.bytesize = 8
+#ser.parity = 'N'
+#ser.stopbits = 1
+#
+#ser.open()
+#if ser.isOpen():
+#    print("Connected to port", ser.name)
+#    ser.write(b'r') #write only accepts bytes
+#    print(ser.readline(256)) #amount of bytes to read are in the xprotolab manual
+#    ser.close()
+#else:
+#    ser.close()
--- xprotolab_controller.py
+++ xprotolab_controller.py
@@ -1,32 +1,41 @@
 #! /usr/bin/env python3
 
 import serial
-import threading
-import time
+#import threading
+#import time
 import sys
 
 #~ print serial.VERSION
 if sys.version_info >= (3, 0):
+    #print(sys.version_info)
     def data(string):
         return bytes(string, 'latin1')
 else:
     def data(string): return string 
 
-class Xprotolab_controller():
-    """Test readline function"""
-    def connect(self):
-       self.ser = serial.Serial()
-       self.ser.baudrate = 115200
-       self.ser.port = '/dev/ttyUSB0'
-       self.ser.bytesize = 8
-       self.ser.parity = 'N'
-       self.ser.stopbits = 1
+class XprotolabController():
+    '''Controller to manage de xprotolab'''
 
-       if ser.isOpen():
-           print("Connected to port", self.ser.name)
+    def __init__(self):
+        self.connect
+
+    def connect(self):
+        print("Connecting to", self.ser.name)
+        self.ser = serial.Serial()
+        self.ser.baudrate = 115200
+        self.ser.port = '/dev/ttyUSB0'
+        self.ser.bytesize = 8
+        self.ser.parity = 'N'
+        self.ser.stopbits = 1
+
+        self.ser.open()
+
+        if ser.isOpen():
+            print("Connected to port", self.ser.name)
        
-       else:
-           self.ser.close()
+        else:
+            print("Can't connect to port")
+            self.ser.close()
 
     def closeConnection(self):
         self.ser.close() 
@@ -35,6 +44,10 @@ class Xprotolab_controller():
         self.ser.write(b'a')
         return self.ser.readline(4)
 
+    def write_settings(self):
+        self.ser.write(b'b')
+
     def restore_factory_settings(self):
         self.ser.write(b'k')
+
 
