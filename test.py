0ee7bf0253933e922c392028cbb00b5e9a965d78
Alvaro Segura Del Barco <alvaro.segura.del-barco@hp.com>
2014-06-12 17:07:14 -0600

    added the test.py file to test if scope is sending data

---
+++ test.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+import serial
+
+#declare serial port and configuration
+#~ print(serial.VERSION)
+ser = serial.Serial()
+ser.baudrate = 115200
+ser.port = '/dev/ttyUSB0'
+ser.bytesize = 8
+ser.parity = 'N'
+ser.stopbits = 1
+
+ser.open()
+if ser.isOpen():
+    print("Connected to port", ser.name)
+    ser.write(b'r') #write only accepts bytes
+    print(ser.readline(256)) #amount of bytes to read are in the xprotolab manual
+    ser.close()
+else:
+    ser.close()
