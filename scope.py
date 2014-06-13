9609af65b910a91d38bfe6e91174a80e23bcdf56
Alvaro Segura Del Barco <alvaro.segura.del-barco@hp.com>
2014-06-13 11:25:44 -0600

    xprotolab class is now querying data

--- scope.py
+++ scope.py
@@ -5,23 +5,6 @@ import xprotolab_controller
 test = xprotolab_controller.XprotolabController()
 
 print(test.__doc__)
-#test.connect
-#print(test.show_version)
-
-#declare serial port and configuration
-#~ print(serial.VERSION)
-#ser = serial.Serial()
-#ser.baudrate = 115200
-#ser.port = '/dev/ttyUSB0'
-#ser.bytesize = 8
-#ser.parity = 'N'
-#ser.stopbits = 1
-#
-#ser.open()
-#if ser.isOpen():
-#    print("Connected to port", ser.name)
-#    ser.write(b'r') #write only accepts bytes
-#    print(ser.readline(256)) #amount of bytes to read are in the xprotolab manual
-#    ser.close()
-#else:
-#    ser.close()
+a = test.show_version()
+print(a)
+print(test.Request_CH1())
--- xprotolab_controller.py
+++ xprotolab_controller.py
@@ -9,30 +9,29 @@ import sys
 if sys.version_info >= (3, 0):
     #print(sys.version_info)
     def data(string):
-        return bytes(string, 'latin1')
+        return bytes(string, 'utf-8')
 else:
     def data(string): return string 
 
-class XprotolabController():
+class XprotolabController:
     '''Controller to manage de xprotolab'''
 
     def __init__(self):
-        self.connect
+        self.ser = serial.Serial()
+        self.connect()
 
     def connect(self):
-        print("Connecting to", self.ser.name)
-        self.ser = serial.Serial()
         self.ser.baudrate = 115200
         self.ser.port = '/dev/ttyUSB0'
         self.ser.bytesize = 8
         self.ser.parity = 'N'
         self.ser.stopbits = 1
+        print("Connecting to", self.ser.name)
 
         self.ser.open()
 
-        if ser.isOpen():
+        if self.ser.isOpen():
             print("Connected to port", self.ser.name)
-       
         else:
             print("Can't connect to port")
             self.ser.close()
@@ -44,10 +43,20 @@ class XprotolabController():
         self.ser.write(b'a')
         return self.ser.readline(4)
 
+    def Request_CH1(self):
+        self.ser.write(b'r')
+        return self.ser.readline(256)
+
+    def Request_CH2(self):
+        self.ser.write(b's')
+        return self.ser.readline(256)
+
+    def Set_Post_Trigger_value(self)
+        self.ser.write(b'j')
+    
     def write_settings(self):
         self.ser.write(b'b')
 
     def restore_factory_settings(self):
         self.ser.write(b'k')
-
 
