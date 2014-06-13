25b581d6223532ceabff53604b12075e415e1074
--mike-- <mejias.michael1@gmail.com>
2014-06-13 17:49:39 -0600

    Adding computePay.py to the master

---
+++ computePay.py
@@ -0,0 +1,41 @@
+#EXERCISE1
+#rewrite your pay computation with time-and-a-half for overtime and create a function called computepay which takes 2 parameter (hours and rate)
+
+def computePay():
+   '''
+   The program take hours and rate, and calculate the ordinary payment and extra payment if apply.
+   Extraordinary payment applies only if hours per week exceed 40 hours, otherwise ordinary payment is applying.
+   The program does not recognize the numbers as string, please enter only numbers 
+   >>>computePay(40,8)
+   >>>320.0
+   >>>ComputePay(45,10)
+   >>>475.0
+   '''
+
+   #http://www.overclock.net/t/342279/tutorial-using-python-glade-to-create-a-simple-gui-application#post3995587
+   #This is the page for the tutorial of the interface
+   extra_pay = 0
+   while True:
+      try:
+         hours = int(raw_input("Please enter a number for hours: \n"))
+         break
+      except ValueError:
+         print "Oops!  That was no valid number. Please enter a valid value for hours..."
+
+   while True:
+      try:
+         rate = int(raw_input("Please enter a number for rate: \n"))
+         break
+      except ValueError:
+         print "Oops!  That was no valid number. Please enter a valid value for rare..."
+   if hours <= 40:
+      ordinary_pay = 40 * rate
+      
+   else:
+      ordinary_pay = 40 * rate
+      extra_pay = (hours - 40) * rate * 1.5
+   
+   total_pay = ordinary_pay + extra_pay
+   return float(total_pay)
+ 
+print computePay()
